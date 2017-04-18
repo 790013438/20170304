139b0bf4100cd8b83a5921cac4c0a7da8dc25e54
790013438 <790013438@qq.com>
2017-04-18 21:07:40 +0800

    看看线程
    不值得看的练习

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,47 +1,10 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Exercise {
 	public static void main(String[]args){
-		DigitSequence demo=new DigitSequence(1729);
-		while(demo.hasNext()){
-			System.out.println(demo.next());
-		}
+		Runnable taskRunnable=() -> {System.out.println("hello");};
+		Executor executor=Executors.newCachedThreadPool();
+		executor.execute(taskRunnable);
 	}
-}
-interface IntSequence{
-	default boolean hasNext(){
-		return true;
-	}
-	int next();
-	public static double average(IntSequence seq,int n){
-		int count=0;
-		double sum=0;
-		while(seq.hasNext()&&count<n){			
-			count++;
-			sum+=seq.next();
-		}
-		return count==0?0:sum/count;
-	}
-}
-class DigitSequence implements IntSequence{
-	private int number;
-	public DigitSequence(int n){
-		number=n;
-	}	
-	public boolean hasNext(){
-		return number!=0;
-	}
-	public int next(){
-		int result=number%10;
-		number/=10;
-		return result;
-	}
-	public int rest(){
-		return number;
-	}
-}
-//½Ó¿ÚÁ·Ï°
-interface Closeable{
-	void close();
-}
-interface Chnnel extends Closeable{
-	boolean isOpen();
 }
